01de95deb64cadfcd13b13d5f0fe788152912c35
seeannjy <jaeyong@seeann.net>
2026-03-09 15:55:39 +0900

    go.mod

--- env/index.go
+++ env/index.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -10,17 +11,11 @@ var AdminPassword string
 var AllowOrigin string
 
 func init() {
-	// .env 파일 없으면 무시
-	_ = godotenv.Load()
-
-	// 환경변수 기본값
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error: Cannot read .env file")
+		panic(err.Error())
+	}
 	AdminPassword = os.Getenv("ADMIN_PASSWORD")
-	if AdminPassword == "" {
-		AdminPassword = "dummy-password"
-	}
-
 	AllowOrigin = os.Getenv("ALLOW_ORIGIN")
-	if AllowOrigin == "" {
-		AllowOrigin = "*"  // 모든 프론트 허용
-	}
 }
