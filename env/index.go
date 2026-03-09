99e6ad045cea356b49533f965fee83bd1bd2dc52
seeannjy <jaeyong@seeann.net>
2026-03-09 15:59:14 +0900

    go.mod

--- env/index.go
+++ env/index.go
@@ -11,11 +11,19 @@ var AdminPassword string
 var AllowOrigin string
 
 func init() {
+	// .env 파일 읽기 시도, 실패해도 그냥 넘어감
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Error: Cannot read .env file")
-		panic(err.Error())
+		fmt.Println("Warning: .env file not found, using defaults")
 	}
+
 	AdminPassword = os.Getenv("ADMIN_PASSWORD")
+	if AdminPassword == "" {
+		AdminPassword = "6692" // 기본값
+	}
+
 	AllowOrigin = os.Getenv("ALLOW_ORIGIN")
+	if AllowOrigin == "" {
+		AllowOrigin = "https://bam0116.github.io" // 기본 CORS 허용 origin
+	}
 }
