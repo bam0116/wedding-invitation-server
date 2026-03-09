166c0a11c8f0e1c51ec99361cbde1f8d5e531e94
seeannjy <jaeyong@seeann.net>
2026-03-09 14:39:17 +0900

    go.mod

--- app.go
+++ app.go
@@ -25,11 +25,16 @@ func main() {
 	mux.Handle("/api/attendance", new(httphandler.AttendanceHandler))
 
 	corHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{env.AllowOrigin},
-		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut},
-		AllowCredentials: true,
-	})
-
+    AllowedOrigins:   []string{env.AllowOrigin},
+    AllowedMethods:   []string{
+        http.MethodGet,
+        http.MethodPost,
+        http.MethodPut,
+        http.MethodOptions, // ← 이거 추가
+    },
+    AllowedHeaders:   []string{"*"}, // 모든 헤더 허용
+    AllowCredentials: true,
+})
 	handler := corHandler.Handler(mux)
 
 	http.ListenAndServe(":8080", handler)
