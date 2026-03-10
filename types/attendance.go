9af98a76f6ef39f43c6ed31f0f6af9c799ebc93a
seeannjy <jaeyong@seeann.net>
2026-03-10 09:21:01 +0900

    go.mod

--- types/attendance.go
+++ types/attendance.go
@@ -1,8 +1,19 @@
 package types
 
+import "time"
+
 type AttendanceCreate struct {
 	Side  string `json:"side"`
 	Name  string `json:"name"`
 	Meal  string `json:"meal"`
 	Count int    `json:"count"`
+}
+
+type Attendance struct {
+    ID        int       `json:"id"`
+    Side      string    `json:"side"`
+    Name      string    `json:"name"`
+    Meal      string    `json:"meal"`
+    Count     int       `json:"count"`
+    CreatedAt time.Time `json:"created_at"`
 }
